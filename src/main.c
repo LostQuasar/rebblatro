ee571863a63344a7454d88c0f9cb0f89f65171ec
LostQuasar <saiph.williams@gmail.com>
2025-03-15 10:42:34 -0400

    Finalize v1

--- src/main.c
+++ src/main.c
@@ -1,78 +1,94 @@
 #include <pebble.h>
-  
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
- 
+
 static GFont s_time_font;
-  
-static void update_time() {
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void update_time()
+{
   // Get a tm structure
-  time_t temp = time(NULL); 
+  time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
- 
+
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H\n%M" : "%I\n%M", tick_time);
- 
+
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
 }
- 
-static void main_window_load(Window *window) {
+
+static void main_window_load(Window *window)
+{
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  
+
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
   // Create time TextLayer
-  s_time_layer = text_layer_create(
-    bounds);
+  s_time_layer = text_layer_create(GRect(0, -7, 144, 168));
   text_layer_set_text(s_time_layer, "00\n00");
-  
+
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_EXAMPLE_FONT_74));
- 
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_M_PLUS_FONT_76));
+
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-  
-  
+
   // Make sure the time is displayed from the start
   update_time();
 }
- 
-static void main_window_unload(Window *window) {
+
+static void main_window_unload(Window *window)
+{
   fonts_unload_custom_font(s_time_font);
-  
+
+  gbitmap_destroy(s_background_bitmap);
+
+  bitmap_layer_destroy(s_background_layer);
   text_layer_destroy(s_time_layer);
 }
- 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
   update_time();
 }
- 
 
-static void init() {
+static void init()
+{
   // Create main Window element and assign to pointer
   s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorBlack);
-    window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+  window_set_window_handlers(s_main_window, (WindowHandlers){
+                                                .load = main_window_load,
+                                                .unload = main_window_unload});
   window_stack_push(s_main_window, true);
-  
+
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
- 
-static void deinit() {
+
+static void deinit()
+{
   // Destroy Window
   window_destroy(s_main_window);
 }
- 
-int main(void) {
+
+int main(void)
+{
   init();
   app_event_loop();
   deinit();
